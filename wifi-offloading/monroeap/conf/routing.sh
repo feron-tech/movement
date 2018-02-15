da137a2c7881a6e9f048872f60b2921a50a7aad1
Antonis Gotsis <antonis.gotsis@gmail.com>
2018-02-15 19:54:09 +0200

    wifi ap: updated parameters

--- wifi-offloading/monroeap/conf/routing.sh
+++ wifi-offloading/monroeap/conf/routing.sh
@@ -4,6 +4,6 @@ iptables --flush
 iptables -A INPUT -s 10.0.0.0/24 -j ACCEPT
 iptables -A OUTPUT -d 10.0.0.0/24 -j ACCEPT
 
-iptables -t nat -A POSTROUTING -o usb0 -j MASQUERADE
-iptables -A FORWARD -i usb0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan0 -o usb0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
